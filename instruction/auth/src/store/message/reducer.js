5fab7469d46f4680cf4324920dd7a249890155c1
Ayse Arzu <aysebattal@gmail.com>
2024-09-24 13:26:19 +0300

    auth1

--- instruction/auth/src/store/message/reducer.js
+++ instruction/auth/src/store/message/reducer.js
@@ -1,9 +1,10 @@
 import { messageState } from "./initial-state";
+import { CLEAR_MESSAGE, SET_MESSAGE } from "./types";
 
 export const messageReducer = (state = messageState, action) => {
-  if (action.type === "SET_MESSAGE") {
+  if (action.type === SET_MESSAGE) {
     return{...state, message : action.payload}
-  }else if(action.type === "CLEAR_MESSAGE"){
+  }else if(action.type === CLEAR_MESSAGE){
     return{...state, message : ""}
   }
     // Bu satir hicbir if bloguna girilmedigi durumlar icin konulur
