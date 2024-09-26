7eb5cc3079765262a52c6bcc1d9712152f73f410
Ayse Arzu <aysebattal@gmail.com>
2024-09-26 12:04:47 +0300

    last lesson

--- practice/practice/src/App.jsx
+++ practice/practice/src/App.jsx
@@ -1,5 +1,4 @@
-
-import { List } from "./components/list";
+/* import { List } from "./components/list";
 import { flowers } from "./assets/flowers.js";
 import { flowers2 } from "./assets/flowers2.js";
 import { Types } from "./components/types.jsx";
@@ -7,12 +6,25 @@ import { Select } from "./components/select.jsx";
 import { ColorPicker } from "./components/colorPicker.jsx";
 import { LoginForm } from "./components/loginForm.jsx";
 import { AddDeleteList } from "./components/06-Add-Delete-List.jsx";
-import { Home } from "./components/07-searchbar-cards/home.jsx";
+import { Header } from "./components/08-api-rooter/header.jsx";
+
+ */
+//import { Home } from "./components/07-searchbar-cards/home.jsx";
+//import { useState } from "react";
+
+import { Users } from "./components/08-api-rooter/users.jsx";
+import { Todos } from "./components/08-api-rooter/todos.jsx";
+import { Home } from "./components/08-api-rooter/home.jsx";
+import { Footer } from "./components/08-api-rooter/footer.jsx";
+
+import { Layout } from "./components/08-api-rooter/layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
+  //let [menuItem, setMenuItem] = useState("Home");
   return (
     <>
-     {/*  <List flowersArr={flowers} />
+      {/*  <List flowersArr={flowers} />
       <List flowersArr={flowers2} />
       <List />
 
@@ -25,10 +37,23 @@ function App() {
       
       <LoginForm />
       <AddDeleteList />
-      
-      */}
       <Home />
-   
+      <Header setMenuItem={setMenuItem} />
+      {menuItem == "Home" && <Home />}
+      {menuItem == "Users" && <Users />}
+      {menuItem == "Todos" && <Todos /> }
+      <Footer />
+      */}
+  <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="users" element={<Users/>}/>
+            <Route path="todos" element={<Todos/>}/>
+          </Route>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </>
   );
 }
---
+++ practice/practice/src/components/08-api-rooter/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='bg-bg-success text-light text-center'>
+        Copyright &copy:2024 by Ayse.
+    </div>
+  )
+}
